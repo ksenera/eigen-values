e1a9073c4c9880a85bf6e40bb5c83a82aa06e6a7
ksenera <ksenera7130@conestogac.on.ca>
2026-03-23 20:18:24 -0400

    remove debugging statement

--- A4.py
+++ A4.py
@@ -34,54 +34,27 @@ def backSub(U, b):
             b[i] = b[i] - U[i][j] * x[j]
     return x
 
-# Gaussian Elimination using Partial Pivoting solving Ax = b
 def gaussElim(A, b):
-    # declare A system and b vector 
     n = len(A)
-    # want to modify only local copies of A sys and b vec 
     A = [row[:] for row in A]
     b = b[:]
-
-    # from ALGO 2.4 for pivoting steps
-    # for k = 1 to n - 1 {loop over cols.}
     for k in range(n-1):
-        # find index p |Apk| >= |Aik| for k <= i <= n {search for pivot in current col}
         p = k
         for i in range(k + 1, n):
             if abs(A[i][k]) > abs(A[p][k]):
                 p = i
-        # if p != k then interchange rows k and p {interchange rows if necessary}
         if p != k:
-
             A[k], A[p] = A[p], A[k]
             b[k], b[p] = b[p], b[k]
-            for row in A:
-                print(row)
-        # in Algo 2.4 this line is line 2 of Algo 2.3 
-        # if akk = 0 then stop {stop if pivot is zero}
-        if A[k][k] == 0:  # if a_kk = 0 then stop
-
+        if abs(A[k][k]) < 1e-12: 
             break
-
-        # for j = k + 1 to n {compute multipliers for current col}
         for i in range(k+1, n):
-            # mik = aik/akk 
             m_ik = A[i][k] / A[k][k]
-            #print(f"Multiplier m_{i}{k} = {m_ik:.2f}")
-
             A[i][k] = 0 
-
-            # for j = k + 1 to n 
             for j in range(k+1, n):
-                # for i = k + 1 to n -> nested for j in for i
-                # {apply transformation to remaining submatrix}
                 A[i][j] = A[i][j] - m_ik * A[k][j] 
-
-            # solving Ly = b (Step 2 page 12 by updating b)
             b[i] = b[i] - m_ik * b[k]
-
-    x = backSub(A, b)
-    return x
+    return backSub(A, b)
 
 # from hints
 def RayleighQuotient(A, x, tolerance):
